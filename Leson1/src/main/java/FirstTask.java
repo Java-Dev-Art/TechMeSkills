712d01c759e1fffb1ef49eec4edd8489091d3274
Artur Markouski <veles.mk.12@gmail.com>
2022-01-19 02:33:04 +0100

    Lesson1 update FirstTask

--- Leson1/src/main/java/FirstTask.java
+++ Leson1/src/main/java/FirstTask.java
@@ -1,21 +1,15 @@
-import java.util.Scanner;
-
 public class FirstTask {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ввидите число");
 
-        int number = scanner.nextInt();
         int count = 0;
-        int i ;
+        for (String arg : args) {
+            int pars = Integer.parseInt(arg);
 
-        System.out.printf("Целые числа : \n");
-
-       for (i= 1; i < number; i++)
-           if (i %2 ==0){
-               count++;
-               System.out.println("\t" + i);
-           }
+            if (pars > 0){
+                count++;
+            }
+        }
         System.out.println("Количество положительных целых чисел равно : " + count);
+
     }
 }
