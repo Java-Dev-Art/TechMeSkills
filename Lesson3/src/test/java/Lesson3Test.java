a2ad7b2249f94cde00c07f337e758f37dd33a7c9
Artur Markouski <veles.mk.12@gmail.com>
2022-02-13 16:15:00 +0100

    Test

---
+++ Lesson3/src/test/java/Lesson3Test.java
@@ -0,0 +1,33 @@
+class Lesson3Test {
+
+    @org.junit.jupiter.api.Test
+    void alphabet() {
+        char[] charEng = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z'};
+        assertEquals();
+    }
+
+    @org.junit.jupiter.api.Test
+    void yourNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void task3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void taskFour() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void taskFive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void average() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void max() {
+    }
+}
