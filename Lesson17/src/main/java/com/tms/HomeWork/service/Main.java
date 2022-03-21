5e070f50f6e1187b329b4026991c95b047a7560b
Artur Markouski <veles.mk.12@gmail.com>
2022-03-21 10:38:44 +0100

    HomeWork/Lesson17

---
+++ Lesson17/src/main/java/com/tms/HomeWork/service/Main.java
@@ -0,0 +1,10 @@
+package com.tms.HomeWork.service;
+
+import com.tms.HomeWork.model.TaskFirst;
+
+public class Main {
+    public static void main(String[] args) {
+        TaskFirst first = new TaskFirst();
+        first.setMyList();
+    }
+}
