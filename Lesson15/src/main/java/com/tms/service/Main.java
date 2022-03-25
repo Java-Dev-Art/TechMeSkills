cd352f3b65ea324bdf5f415a16b7449b6d5bb71f
Artur Markouski <veles.mk.12@gmail.com>
2022-03-25 19:03:01 +0100

    HomeWork/Lesson15

--- Lesson15/src/main/java/com/tms/service/Main.java
+++ Lesson15/src/main/java/com/tms/service/Main.java
@@ -1,14 +1,18 @@
 package com.tms.service;
 
-import com.tms.model.ReadMyFile;
-
-import java.io.File;
+import com.tms.model.MinMax;
 
 public class Main {
     private final static String SOURCE = "/home/artur/TechmeSkills/TechMeSkills/Lesson15/src/";
 
     public static void main(String[] args) {
-        ReadMyFile readMyFile = new ReadMyFile();
-        readMyFile.read(new File(SOURCE));
+//        ReadMyFile readMyFile = new ReadMyFile();
+//        readMyFile.read(new File(SOURCE));
+//        Animal fox =new Animal("FOX");
+//        SomeClass someClass = new SomeClass(45,fox,4);
+//        someClass.getNameClass();
+        Integer[] my = {1, 4, 6, 70, 0};
+        MinMax minMax = new MinMax(my);
+        minMax.getMin();
     }
 }
