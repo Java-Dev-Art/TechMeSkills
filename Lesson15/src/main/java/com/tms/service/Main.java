4bd1cf86fedc7195a62a6dfc7fde3144d339c5fe
Artur Markouski <veles.mk.12@gmail.com>
2022-03-26 12:39:57 +0100

    HomeWork/Lesson15

--- Lesson15/src/main/java/com/tms/service/Main.java
+++ Lesson15/src/main/java/com/tms/service/Main.java
@@ -1,6 +1,6 @@
 package com.tms.service;
 
-import com.tms.model.MinMax;
+import com.tms.model.Calculator;
 
 public class Main {
     private final static String SOURCE = "/home/artur/TechmeSkills/TechMeSkills/Lesson15/src/";
@@ -11,8 +11,46 @@ public class Main {
 //        Animal fox =new Animal("FOX");
 //        SomeClass someClass = new SomeClass(45,fox,4);
 //        someClass.getNameClass();
-        Integer[] my = {1, 4, 6, 70, 0};
-        MinMax minMax = new MinMax(my);
-        minMax.getMin();
+//        Integer[] my = {1, 4, 6, 70, 0};
+//        MinMax minMax = new MinMax(my);
+//        minMax.getMin();
+
+//        System.out.println(Calculator.sum(1,2));
+//        System.out.println(Calculator.sum(10.5,25.6));
+//        System.out.println(Calculator.sum(1,2.6));
+//        System.out.println(Calculator.sum(1.9,2));
+//        String s = "8";
+//        System.out.println(Calculator.sum("9",s));
+//        System.out.println(Calculator.sum(null,2));
+//        System.out.println(Calculator.sum(1,null));
+
+//        System.out.println(Calculator.multiply(1,2));
+//        System.out.println(Calculator.multiply(10.5,25.6));
+//        System.out.println(Calculator.multiply(1,2.6));
+//        System.out.println(Calculator.multiply(1.9,2));
+////        String s = "8";
+////        System.out.println(Calculator.multiply("9",s));
+//        System.out.println(Calculator.multiply(null,2));
+//        System.out.println(Calculator.multiply(1,null));
+
+//        System.out.println(Calculator.divide(1,2));
+//        System.out.println(Calculator.divide(10.5,25.6));
+//        System.out.println(Calculator.divide(1,2.6));
+//        System.out.println(Calculator.divide(1.9,2));
+////        String s = "8";
+////        System.out.println(Calculator.divide("9",s));
+//        System.out.println(Calculator.divide(null,2));
+//        System.out.println(Calculator.divide(1,null));
+
+        System.out.println(Calculator.subtraction(1, 2));
+        System.out.println(Calculator.subtraction(10.5, 25.6));
+        System.out.println(Calculator.subtraction(1, 2.6));
+        System.out.println(Calculator.subtraction(1.9, 2));
+//        String s = "8";
+//        System.out.println(Calculator.divide("9",s));
+        System.out.println(Calculator.subtraction(null, 2));
+        System.out.println(Calculator.subtraction(1, null));
+
+
     }
 }
