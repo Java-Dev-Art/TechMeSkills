2257a72c55f3f7af29886f7a1fa82c1b3f3a813a
Artur Markouski <veles.mk.12@gmail.com>
2022-03-26 22:01:05 +0100

    HomeWork/Lesson15

--- Lesson13/src/main/java/com/tms/model/TextFormat.java
+++ Lesson13/src/main/java/com/tms/model/TextFormat.java
@@ -12,4 +12,14 @@ public class TextFormat {
         char second = text.charAt(0);
         return text.replace(first, second);
     }
+
+    public void pollidrome(String text) {
+        String st = text.replaceAll(",", " ");
+        String[] massStr = st.split(" ");
+        for (String str : massStr) {
+            if (str.equalsIgnoreCase(String.valueOf(new StringBuilder(str).reverse()))) {
+                System.out.println(str);
+            }
+        }
+    }
 }
--- Lesson13/src/main/java/com/tms/service/Main.java
+++ Lesson13/src/main/java/com/tms/service/Main.java
@@ -4,11 +4,15 @@ import com.tms.model.TextFormat;
 
 public class Main {
     public static void main(String[] args) {
-        String source = "Вырезать подстроку из строки начиная с первого вхождения символа(А) до, последнего вхождения сивола(B)";
+        String source = "Вырезать подстроку из строки начиная с первого вхождения символа(А) до, последнего вхождения сивола(B) " +
+                "txt, local, mart подстроку из строки " +
+                "начиная с mam pppp заказ, казак, дед";
+
         TextFormat textFormat = new TextFormat();
         System.out.println(textFormat.cutSomeChar(source, ' ', 'а'));
         System.out.println();
         System.out.println(textFormat.replaceSymbol("text"));
+        textFormat.pollidrome(source);
 
     }
 }
