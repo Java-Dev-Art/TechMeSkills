1dece66c0aa4b14b7d43c84e9bd5007dcd6dd1b8
Artur Markouski <veles.mk.12@gmail.com>
2022-04-24 13:27:28 +0200

    Lesson23/

---
+++ Lesson23/src/main/java/com/tms/servlets/NewsServlet.java
@@ -0,0 +1,22 @@
+package com.tms.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/news")
+public class NewsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
+        System.out.println("start get");
+        resp.setContentType("text/html");
+        String site = "https://www.zerkalo.io/";
+        resp.setStatus(resp.SC_ACCEPTED);
+        resp.setHeader("Location", site);
+        resp.sendRedirect(site);
+    }
+}
